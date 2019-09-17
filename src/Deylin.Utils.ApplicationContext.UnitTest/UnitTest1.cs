271215958908ce9f15ebf7c6f97158c53d4da7c8
Barry de Boer <h.l.de.boer@umcg.nl>
2019-09-17 19:56:29 +0200

    Updated unit tests

--- src/Deylin.Utils.ApplicationContext.UnitTest/UnitTest1.cs
+++ src/Deylin.Utils.ApplicationContext.UnitTest/UnitTest1.cs
@@ -11,24 +11,28 @@ namespace Deylin.Utils.ApplicationContext.UnitTest
 
 
         [TestMethod]
-        public void TestConfiguration()
+        public void TestConfiguration1()
         {
             try
             {
-                this.appctx = ApplicationContext.Current.Instance;
-                this.appctx.OnException += (obj, exc) => Assert.Fail(exc.Message);
-                this.config = this.appctx.GetConfigurationData(Configuration.Settings.Current);
+                using (this.appctx = ApplicationContext.Current.Instance)
+                {
+                    void handler(object obj, Exception exc) => Assert.Fail(exc.Message);
 
-                
-                Assert.AreEqual(new Guid("2964204F-0041-47FF-BA16-D7D4220B6EB5"), this.config.GUID);
-                Assert.AreEqual("Deylin.Utils.ApplicationContext.UnitTests", this.config.ApplicationName);
-                Assert.AreEqual(ApplicationContext.Configuration.EnvironmentTypes.Development, this.config.Environment);
-                Assert.AreEqual("LocalRelease", this.config.Release);
-                Assert.AreEqual(new Version(1,2,3,4), this.config.Version);
-                Assert.AreEqual(typeof(Configuration.UnitTestClass), this.config.TestClass.GetType());
-                Assert.AreEqual("qwerty", this.config.TestClass.TestString);                
+                    this.appctx.OnException += handler;
+                    this.config = this.appctx.GetConfigurationData(Configuration.Settings.Current);
 
-                this.appctx.Dispose();
+
+                    Assert.AreEqual(new Guid("2964204F-0041-47FF-BA16-D7D4220B6EB5"), this.config.GUID);
+                    Assert.AreEqual("Deylin.Utils.ApplicationContext.UnitTests", this.config.ApplicationName);
+                    Assert.AreEqual(ApplicationContext.Configuration.EnvironmentTypes.Development, this.config.Environment);
+                    Assert.AreEqual("LocalRelease", this.config.Release);
+                    Assert.AreEqual(new Version(1, 2, 3, 4), this.config.Version);
+                    Assert.AreEqual(typeof(Configuration.UnitTestClass), this.config.TestClass.GetType());
+                    Assert.AreEqual("qwerty", this.config.TestClass.TestString);
+
+                    this.appctx.OnException -= handler;
+                }
             }
             catch(Exception exc)
             {
@@ -37,22 +41,43 @@ namespace Deylin.Utils.ApplicationContext.UnitTest
         }
 
         [TestMethod]
+        public void TestConfiguration2()
+        {
+            using (this.appctx = ApplicationContext.Current.Instance)
+            {
+                void handler(object obj, Exception exc)
+                {
+                    Console.WriteLine($"Exception thrown in TestConfiguration2: {exc.Message}");
+
+                    Assert.IsTrue(exc.Message.StartsWith("Could not find file"));
+                }
+                this.appctx.OnException += handler;
+                
+                this.config = this.appctx.GetConfigurationData(Configuration.Settings2.Current);
+                Assert.IsNull(this.config);
+                this.appctx.OnException -= handler;
+            }
+        }
+
+        [TestMethod]
         public void TestLogging()
         {
             try
             {
-                this.appctx = ApplicationContext.Current.Instance;
-                this.appctx.OnException += (obj, exc) => Assert.Fail(exc.Message);
+                using (this.appctx = ApplicationContext.Current.Instance)
+                {
+                    void handler(object obj, Exception exc) => Assert.Fail(exc.Message);
+                    this.appctx.OnException += handler;
 
-                this.appctx.Log(Logging.Level.Debug, "DebugMessage");
-                this.appctx.Log(Logging.Level.Error, "ErrorMessage");
-                this.appctx.Log(Logging.Level.Fatal, "FatalMessage", new Exception("UnitTestException"));
-                this.appctx.Log(Logging.Level.Info, "InfoMessage");
-                this.appctx.Log(Logging.Level.Trace, "TraceMessage");
-                this.appctx.Log(Logging.Level.Warning, "WarningMessage");
+                    this.appctx.Log(Logging.Level.Debug, "DebugMessage");
+                    this.appctx.Log(Logging.Level.Error, "ErrorMessage");
+                    this.appctx.Log(Logging.Level.Fatal, "FatalMessage", new Exception("UnitTestException"));
+                    this.appctx.Log(Logging.Level.Info, "InfoMessage");
+                    this.appctx.Log(Logging.Level.Trace, "TraceMessage");
+                    this.appctx.Log(Logging.Level.Warning, "WarningMessage");
 
-
-                this.appctx.Dispose();
+                    this.appctx.OnException -= handler;
+                }
             }
             catch (Exception exc)
             {
@@ -60,5 +85,6 @@ namespace Deylin.Utils.ApplicationContext.UnitTest
             }
         }
 
+        
     }
 }
